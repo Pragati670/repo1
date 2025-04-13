bc4044410a59320fb013c2a587773e0e0467b030
Pragati670 <pragsagrs@gmail.com>
2025-04-13 06:36:33 +0000

    Automate build

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello,I am Atul  and this is my project to deploying through CI/CD pileline V1.0!"
+    return "Hello,I am Pragati and I am new to this project  and this is my project to deploying through CI/CD pileline V1.0!"
 
 if __name__ == "__main__":
     app.run(host ='0.0.0.0', port= 8080)
